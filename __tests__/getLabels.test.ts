cbb9c0793f0035aedba7a6174baf445685c5e7cb
César M. Cristóbal <cmartin@monom.ai>
2022-09-27 09:22:05 +0200

    refactor getlabels, new doRequest function

--- __tests__/getLabels.test.ts
+++ __tests__/getLabels.test.ts
@@ -5,11 +5,20 @@ const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 })
 
+const nooctokit = new Octokit({
+    auth: '1234567890'
+})
+
 const prNumber = '15'
 const repoName = 'monom-manifests'
 const owner = 'ThingsO2'
 
-test('getLabels', async () => {
+test('get labels', async () => {
     const labels = await getLabels(octokit, prNumber, repoName, owner)
-    expect(labels).toEqual([{"color": "ededed", "default": false, "description": null, "id": 4323254495, "name": "test", "node_id": "LA_kwDOHR6z9M8AAAABAa-g3w", "url": "https://api.github.com/repos/ThingsO2/monom-manifests/labels/test"}])
+    expect(labels).toEqual(['test'])
+})
+
+test('get labels not found', async () => {
+    const labels = await getLabels(nooctokit, prNumber, repoName, owner)
+    expect(labels).toBeUndefined()
 })
--- src/getLabels.ts
+++ src/getLabels.ts
@@ -1,19 +1,36 @@
+import * as core from '@actions/core'
 import { Octokit } from "@octokit/core"
-import { Endpoints } from "@octokit/types";
+import { Endpoints } from "@octokit/types"
 
 type listLabelsOnIssueResponse = Endpoints["GET /repos/{owner}/{repo}/issues/{issue_number}/labels"]["response"]
 
-export async function getLabels(octokit: Octokit, prNumber: string, repoName: string, owner: string): Promise<listLabelsOnIssueResponse> {
+export async function getLabels(octokit: Octokit, prNumber: string, repoName: string, owner: string): Promise<string[] | undefined> {
+
+    const res = await doRequest(octokit, prNumber, repoName, owner)
+
+    if (res.status === 200) {
+        return res.data.map((label) => label.name)
+    }
+    core.error(`Error: ${res}`)
+    return undefined
+}
+
+async function doRequest(octokit: Octokit, prNumber: string, repoName: string, owner: string): Promise<listLabelsOnIssueResponse> {
 
     const request = `GET /repos/${owner}/${repoName}/issues/${prNumber}/labels`
 
     try {
-        const {data: res } = await octokit.request(request, {
+        const {data: res} = await octokit.request(request, {
             owner: owner,
             repo: repoName,
             issue_number: prNumber
           })
-        return res
+        return {
+            data: res,
+            status: 200,
+            url: request,
+            headers: {}
+        }
     } catch (error) {
         return error
     }
