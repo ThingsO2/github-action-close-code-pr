7b6b06567ae1f6441cb0684a8c2388a216b1ae23
César M. Cristóbal <cmartin@monom.ai>
2022-09-28 12:20:44 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -8015,43 +8015,47 @@ var core_1 = __nccwpck_require__(6762);
 var getLabels_1 = __nccwpck_require__(7140);
 var searchPRwithLabels_1 = __nccwpck_require__(3595);
 var searchCodePR_1 = __nccwpck_require__(5433);
-var main = function (input) {
+var mergePR_1 = __nccwpck_require__(6964);
+var main = function (octokit, input, merge) {
     var prNumber = input.prNumber;
     var repoName = input.repoName;
     var owner = input.owner;
     core.info("PR Number: ".concat(prNumber));
     core.info("Repo Name: ".concat(repoName));
-    var octokit = new core_1.Octokit({
-        auth: process.env.GITHUB_TOKEN
-    });
-    (0, getLabels_1.getLabels)(octokit, prNumber, repoName, owner).then(function (labels) {
+    return (0, getLabels_1.getLabels)(octokit, prNumber, repoName, owner).then(function (labels) {
         core.info("Labels: ".concat(labels));
         if (labels === undefined) {
             core.setFailed("Request label failed");
+            return core.ExitCode.Failure;
         }
-        (0, searchPRwithLabels_1.searchPRwithLabels)(octokit, repoName, owner, labels).then(function (PRs) {
+        return (0, searchPRwithLabels_1.searchPRwithLabels)(octokit, repoName, owner, labels).then(function (PRs) {
             core.info("PRs: ".concat(PRs));
             if (PRs === undefined) {
                 core.setFailed("Request PRs failed");
+                return core.ExitCode.Failure;
             }
             if (PRs.length > 0) {
                 core.info('PRs found, nothing to merge');
-                core.ExitCode.Success;
+                return core.ExitCode.Success;
             }
             else {
                 core.info('No PRs found, merge original code PR');
-                (0, searchCodePR_1.searchCodePR)(octokit, prNumber, repoName, owner).then(function (codePR) {
+                return (0, searchCodePR_1.searchCodePR)(octokit, prNumber, repoName, owner).then(function (codePR) {
                     core.info("Repo: ".concat(codePR.base.repo.name, " PR: ").concat(codePR === null || codePR === void 0 ? void 0 : codePR.number));
                     if (codePR === undefined) {
                         core.setFailed("Request code PR failed");
+                        return core.ExitCode.Failure;
                     }
-                    //mergePR(octokit, codePR.number.toString(), codePR.base.repo.name, owner).then((mergeResult) => {
-                    //    core.info(`Merge Result: ${mergeResult}`)
-                    //    if (mergeResult === undefined) {
-                    //        core.setFailed(`Merge PR failed`)
-                    //    }
-                    //    core.ExitCode.Success
-                    //})
+                    if (merge) {
+                        return (0, mergePR_1.mergePR)(octokit, codePR.number.toString(), codePR.base.repo.name, owner).then(function (mergeResult) {
+                            core.info("Merge Result: ".concat(mergeResult));
+                            if (mergeResult === undefined) {
+                                core.setFailed("Merge PR failed");
+                            }
+                            return core.ExitCode.Success;
+                        });
+                    }
+                    return core.ExitCode.Success;
                 });
             }
         });
@@ -8062,10 +8066,110 @@ try {
     var prNumber = core.getInput('pr_number');
     var repoName = core.getInput('repo_name');
     var owner = core.getInput('owner');
-    (0, exports.main)({ prNumber: prNumber, repoName: repoName, owner: owner });
+    var octokit = new core_1.Octokit({
+        auth: process.env.GITHUB_TOKEN
+    });
+    (0, exports.main)(octokit, { prNumber: prNumber, repoName: repoName, owner: owner }, false);
 }
 catch (error) {
     core.setFailed(error.message);
+}
+
+
+/***/ }),
+
+/***/ 6964:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.mergePR = void 0;
+var core = __nccwpck_require__(2186);
+function mergePR(octokit, prNumber, repoName, owner) {
+    return __awaiter(this, void 0, void 0, function () {
+        var res;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, doRequest(octokit, prNumber, repoName, owner)];
+                case 1:
+                    res = _a.sent();
+                    if (res.status === 200) {
+                        return [2 /*return*/, res.data];
+                    }
+                    core.error("Error: ".concat(res));
+                    return [2 /*return*/, undefined];
+            }
+        });
+    });
+}
+exports.mergePR = mergePR;
+function doRequest(octokit, prNumber, repoName, owner) {
+    return __awaiter(this, void 0, void 0, function () {
+        var request, res, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    request = "PUT /repos/".concat(owner, "/").concat(repoName, "/pulls/").concat(prNumber, "/merge");
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, octokit.request(request, {
+                            owner: owner,
+                            repo: repoName,
+                            pull_number: prNumber,
+                        })];
+                case 2:
+                    res = (_a.sent()).data;
+                    return [2 /*return*/, {
+                            data: res,
+                            status: 200,
+                            url: request,
+                            headers: {}
+                        }];
+                case 3:
+                    error_1 = _a.sent();
+                    return [2 /*return*/, error_1];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
 }
 
 
